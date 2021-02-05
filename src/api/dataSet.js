35acf7641a402f41bf9715ade66bb91a8978dba1
Laviza Falak Naz <60230431+LavizaFalakNaz@users.noreply.github.com>
2021-02-06 04:11:30 +0500

    Create dataSet.js

---
+++ src/api/dataSet.js
@@ -0,0 +1,24 @@
+const dataSet = [
+  {
+    question: "What is the Capital city of Pakistan?",
+    options: ["Karachi", "Islamabad", "Lahore"],
+    correct: 0
+  },
+  {
+    question: "What is the Capital city of Iran?",
+    options: ["Kabul", "Damam", "Tehran"],
+    correct: 2
+  },
+  {
+    question: "What is the Capital city of Hungary?",
+    options: ["Budapest", "Pisa", "Rome"],
+    correct: 0
+  },
+  {
+    question: "What is the Capital city of USA?",
+    options: ["San Fransisco", "Los Angeles", "Washington DC"],
+    correct: 2
+  }
+];
+
+export default dataSet;
